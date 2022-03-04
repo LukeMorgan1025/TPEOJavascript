1baeec72973ea673385702272575fff0ffd099f0
LukeMorgan1025 <lukealexmorgan@gmail.com>
2022-03-03 19:00:43 -0600

    Demo

---
+++ Homework_SumOfSquareAreas.js
@@ -0,0 +1,11 @@
+function sumOfSquareAreas (sideLength1, sideLength2) {
+    return (sideLength1 * sideLength1) + (sideLength2 * sideLength2);
+}
+
+area1 = sumOfSquareAreas(9, 3);
+area2 = sumOfSquareAreas(4, 6);
+area3 = sumOfSquareAreas(7, 11);
+
+console.log(area1);
+console.log(area2);
+console.log(area3);
---
+++ Practice_ConvertFeetToInches.js
@@ -0,0 +1,11 @@
+function convertFeetToInches (lengthInFeet) {
+    return lengthInFeet * 12;
+}
+
+lengthInFeet1 = convertFeetToInches(1);
+lengthInFeet2 = convertFeetToInches(3);
+lengthInFeet3 = convertFeetToInches(30);
+
+console.log(lengthInFeet1);
+console.log(lengthInFeet2);
+console.log(lengthInFeet3);
